225875bae28700453de33d6e5ce9271ed3a3460e
Riya Savant <rieee2110@gmail.com>
2021-03-19 20:05:11 +0530

    Completed: Registration for Adopt Users

--- lib/authentication/adopt_register.dart
+++ lib/authentication/adopt_register.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:pawfecto/authentication/adopt_login.dart';
 import 'package:pawfecto/components/rounded_button.dart';
 import 'package:pawfecto/constants.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:pawfecto/authentication/adopt_main.dart';
 
 class AdoptRegister extends StatefulWidget {
   static const String id = 'adopt_register';
@@ -10,6 +12,11 @@ class AdoptRegister extends StatefulWidget {
 }
 
 class _AdoptRegisterState extends State<AdoptRegister> {
+  // private instance auth, which will be used to register
+  final _auth = FirebaseAuth.instance;
+  String email;
+  String password;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -47,39 +54,46 @@ class _AdoptRegisterState extends State<AdoptRegister> {
                   SizedBox(
                     height: 38.0,
                   ),
+                  // TextFormField(
+                  //   decoration: textinputdecoration.copyWith(
+                  //     hintText: 'Enter Name',
+                  //     labelText: 'NAME',
+                  //   ),
+                  // ),
+                  // SizedBox(
+                  //   height: 15.0,
+                  // ),
                   TextFormField(
-                    decoration: textinputdecoration.copyWith(
-                      hintText: 'Enter Name',
-                      labelText: 'NAME',
-                    ),
-                  ),
-                  SizedBox(
-                    height: 15.0,
-                  ),
-                  TextFormField(
+                    keyboardType: TextInputType.emailAddress,
                     decoration: textinputdecoration.copyWith(
                       hintText: 'Enter Email',
                       labelText: 'EMAIL',
                     ),
+                    onChanged: (value) {
+                      email = value;
+                    },
                   ),
                   SizedBox(
                     height: 15.0,
                   ),
-                  TextFormField(
-                    decoration: textinputdecoration.copyWith(
-                      hintText: 'Enter Phone Number',
-                      labelText: 'PHONE',
-                    ),
-                  ),
-                  SizedBox(
-                    height: 15.0,
-                  ),
+                  // TextFormField(
+                  //   decoration: textinputdecoration.copyWith(
+                  //     hintText: 'Enter Phone Number',
+                  //     labelText: 'PHONE',
+                  //   ),
+                  // ),
+                  // SizedBox(
+                  //   height: 15.0,
+                  // ),
                   TextFormField(
                     obscureText: true,
                     decoration: textinputdecoration.copyWith(
                       hintText: 'Enter Password',
                       labelText: 'PASSWORD',
                     ),
+                    onChanged: (value) {
+                      password = value;
+                    },
                   ),
                   SizedBox(
                     height: 20.0,
@@ -88,8 +102,20 @@ class _AdoptRegisterState extends State<AdoptRegister> {
                     title: 'REGISTER',
                     colour: Color.fromARGB(255, 0, 136, 145),
                     tcolor: Colors.white,
-                    onPressed: () {
-                      // must register to firebase
+                    onPressed: () async {
+                      // to catch errors such as already registered user
+                      try {
+                        // returns a Future
+                        final newUser =
+                            await _auth.createUserWithEmailAndPassword(
+                                email: email, password: password);
+
+                        if (newUser != null) {
+                          Navigator.pushNamed(context, AdoptMain.id);
+                        }
+                      } catch (e) {
+                        print(e);
+                      }
                     },
                   ),
                   Container(
