3788635aa97266f9e1f0866a873d4cd23fce4935
Riya Savant <rieee2110@gmail.com>
2021-03-19 20:03:44 +0530

    Adopt User screen after login/registration

---
+++ lib/authentication/adopt_main.dart
@@ -0,0 +1,59 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AdoptMain extends StatefulWidget {
+  static const String id = 'adopt_main';
+  @override
+  _AdoptMainState createState() => _AdoptMainState();
+}
+
+class _AdoptMainState extends State<AdoptMain> {
+  final _auth = FirebaseAuth.instance;
+
+  // FirebaseUser class was changed to User
+  User loggedInUser;
+
+  @override
+  void initState() {
+    super.initState();
+
+    getCurrentUser();
+  }
+
+  void getCurrentUser() {
+    try {
+      final user = _auth.currentUser;
+      if (user != null) {
+        loggedInUser = user;
+        print(loggedInUser.email);
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        top: true,
+        bottom: true,
+        right: true,
+        left: true,
+        child: Center(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.symmetric(vertical: 50.0, horizontal: 40.0),
+              child: Container(
+                child: Image.asset('images/pawsfecto.png'),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
