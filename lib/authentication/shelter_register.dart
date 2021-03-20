9363285a2c94ed23795b239f089ec97b02e05997
Riya Savant <rieee2110@gmail.com>
2021-03-20 12:40:31 +0530

    Shelter: Stores registered data into firestore

--- lib/authentication/shelter_register.dart
+++ lib/authentication/shelter_register.dart
@@ -5,6 +5,7 @@ import 'package:pawfecto/components/rounded_button.dart';
 import 'package:pawfecto/constants.dart';
 import 'package:modal_progress_hud/modal_progress_hud.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class SRegister extends StatefulWidget {
   static const String id = 's_register';
@@ -13,9 +14,12 @@ class SRegister extends StatefulWidget {
 }
 
 class _SRegisterState extends State<SRegister> {
+  final _firestore = FirebaseFirestore.instance;
   final _auth = FirebaseAuth.instance;
   bool isLoading = false;
   String email;
+  String shelterName;
+  String phone;
   String password;
 
   @override
@@ -57,15 +61,18 @@ class _SRegisterState extends State<SRegister> {
                     SizedBox(
                       height: 38.0,
                     ),
-                    // TextFormField(
-                    //   decoration: textinputdecoration.copyWith(
-                    //     hintText: 'Enter Shelter Name',
-                    //     labelText: 'SHELTER NAME',
-                    //   ),
-                    // ),
-                    // SizedBox(
-                    //   height: 15.0,
-                    // ),
+                    TextFormField(
+                      decoration: textinputdecoration.copyWith(
+                        hintText: 'Enter Shelter Name',
+                        labelText: 'SHELTER NAME',
+                      ),
+                      onChanged: (value) {
+                        shelterName = value;
+                      },
+                    ),
+                    SizedBox(
+                      height: 15.0,
+                    ),
                     TextFormField(
                       decoration: textinputdecoration.copyWith(
                         hintText: 'Enter Email',
@@ -78,15 +85,18 @@ class _SRegisterState extends State<SRegister> {
                     SizedBox(
                       height: 15.0,
                     ),
-                    // TextFormField(
-                    //   decoration: textinputdecoration.copyWith(
-                    //     hintText: 'Enter Phone Number',
-                    //     labelText: 'PHONE',
-                    //   ),
-                    // ),
-                    // SizedBox(
-                    //   height: 15.0,
-                    // ),
+                    TextFormField(
+                      decoration: textinputdecoration.copyWith(
+                        hintText: 'Enter Phone Number',
+                        labelText: 'PHONE',
+                      ),
+                      onChanged: (value) {
+                        phone = value;
+                      },
+                    ),
+                    SizedBox(
+                      height: 15.0,
+                    ),
                     TextFormField(
                       obscureText: true,
                       decoration: textinputdecoration.copyWith(
@@ -120,6 +130,16 @@ class _SRegisterState extends State<SRegister> {
                             Navigator.pushNamed(context, ShelterMain.id);
                           }
 
+                          print(user);
+
+                          // save data to cloud firestore
+                          _firestore.collection('shelters').add({
+                            'name': shelterName,
+                            'email': email,
+                            'phone': phone,
+                            'pets': [],
+                          });
+
                           // set spinner to false
                           setState(() {
                             isLoading = false;
@@ -131,16 +151,29 @@ class _SRegisterState extends State<SRegister> {
                     ),
                     Container(
                       alignment: Alignment.center,
-                      child: GestureDetector(
-                          child: Text("Already a user? Login",
-                              textAlign: TextAlign.center,
-                              style: TextStyle(
-                                  fontSize: 17,
-                                  decoration: TextDecoration.underline,
-                                  color: Color.fromARGB(255, 0, 136, 145))),
-                          onTap: () {
-                            Navigator.pushNamed(context, ShelterLogin.id);
-                          }),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Already a user? ',
+                            style: TextStyle(
+                              fontSize: 17,
+                              // decoration: TextDecoration.underline,
+                              color: Colors.grey,
+                            ),
+                          ),
+                          GestureDetector(
+                              child: Text("Login",
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                      fontSize: 17,
+                                      // decoration: TextDecoration.underline,
+                                      color: Color.fromARGB(255, 0, 136, 145))),
+                              onTap: () {
+                                Navigator.pushNamed(context, ShelterLogin.id);
+                              }),
+                        ],
+                      ),
                     ),
                   ],
                 ),
