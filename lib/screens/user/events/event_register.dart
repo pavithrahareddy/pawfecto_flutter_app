edc039e54e9247ed8ff0babb4d375f99f1c79abb
Riya Savant <rieee2110@gmail.com>
2021-04-09 13:03:28 +0530

    Modified Event registration flow / added email notification

--- lib/screens/user/events/event_register.dart
+++ lib/screens/user/events/event_register.dart
@@ -1,6 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:mailer/mailer.dart';
+import 'package:mailer/smtp_server.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
+
 import 'package:pawfecto/screens/user/adopt_main.dart';
 import 'package:pawfecto/screens/user/events/event_details.dart';
 
@@ -24,8 +29,12 @@ class _EventRegisterState extends State<EventRegister> {
   String _email = "";
 
   bool isRegistered = false;
+  bool isLoading = false;
 
   void registerUser() async {
+    setState(() {
+      isLoading = true;
+    });
     try {
       final shelter = await _firestore
           .collection('shelters')
@@ -66,8 +75,39 @@ class _EventRegisterState extends State<EventRegister> {
       );
       setState(() {
         isRegistered = true;
+        isLoading = false;
       });
+      sendEmailNotification();
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(
+            'Confirmation Email Sent',
+          ),
+        ),
+      );
     } catch (e) {
+      print(e);
+    }
+  }
+
+  void sendEmailNotification() async {
+    String username = env['EMAIL'];
+    String password = env['PASSWORD'];
+
+    final smtpServer = gmail(username, password);
+
+    final message = Message()
+      ..from = Address(username, 'Pawfecto')
+      ..recipients.add(_email)
+      ..subject = 'Registration Confirmed'
+      ..text =
+          'Hi ${_name},.\n\nThis is to notify you that your registration has been confirmed.\nEvent name: ${this.widget.event["name"]}\nLocation: ${this.widget.event["location"]}\nDate: ${this.widget.event["date"]}\nTime: ${this.widget.event["time"]}\n\nTeam Pawfecto';
+
+    try {
+      final sendReport = await send(message, smtpServer);
+      print('Message sent: ' + sendReport.toString());
+    } on MailerException catch (e) {
+      print('Message not sent.');
       print(e);
     }
   }
@@ -76,204 +116,207 @@ class _EventRegisterState extends State<EventRegister> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: SafeArea(
-        child: isRegistered
-            ? Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Image(
-                      image: AssetImage('images/reg_success.jpg'),
-                      height: 200.0,
-                      width: 200.0,
-                    ),
-                    SizedBox(
-                      height: 10.0,
-                    ),
-                    Text(
-                      'Registered Successfully!',
-                      style: TextStyle(
-                        fontSize: 20.0,
+      body: ModalProgressHUD(
+        inAsyncCall: isLoading,
+        child: SafeArea(
+          child: isRegistered
+              ? Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image(
+                        image: AssetImage('images/reg_success.jpg'),
+                        height: 200.0,
+                        width: 200.0,
                       ),
-                    ),
-                    SizedBox(
-                      height: 20.0,
-                    ),
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.pushNamedAndRemoveUntil(
-                            context, AdoptMain.id, (route) => false);
-                      },
-                      style: ButtonStyle(
-                          backgroundColor: MaterialStateProperty.all<Color>(
-                              Color(0xff008891))),
-                      child: Text(
-                        'HOME',
+                      SizedBox(
+                        height: 10.0,
                       ),
-                    )
-                  ],
-                ),
-              )
-            : SingleChildScrollView(
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(right: 16.0, top: 8.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          GestureDetector(
-                            child: Icon(
-                              Icons.close,
-                            ),
-                            onTap: () {
-                              Navigator.popAndPushNamed(
-                                  context, EventDetails.id);
-                            },
-                          ),
-                        ],
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 30.0),
-                      child: Text(
-                        'Event Register',
+                      Text(
+                        'Registered Successfully!',
                         style: TextStyle(
-                          fontSize: 30.0,
-                          color: Color(0xff008891),
+                          fontSize: 20.0,
                         ),
                       ),
-                    ),
-                    Form(
-                      key: _formKey,
-                      child: Padding(
-                        padding: const EdgeInsets.all(25.0),
-                        child: Column(
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.pushNamedAndRemoveUntil(
+                              context, AdoptMain.id, (route) => false);
+                        },
+                        style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all<Color>(
+                                Color(0xff008891))),
+                        child: Text(
+                          'HOME',
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              : SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(right: 16.0, top: 8.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
                           children: [
-                            TextFormField(
-                              decoration: InputDecoration(
-                                // filled: true,
-                                labelText: 'Name',
-                                labelStyle: TextStyle(
-                                  color: Colors.grey,
-                                ),
-                                enabledBorder: UnderlineInputBorder(
-                                  borderSide: BorderSide(
-                                    color: Colors.grey,
-                                  ),
-                                ),
-                                focusedBorder: UnderlineInputBorder(
-                                  borderSide: BorderSide(
-                                    color: Colors.grey,
-                                  ),
-                                ),
+                            GestureDetector(
+                              child: Icon(
+                                Icons.close,
                               ),
-                              // The validator receives the text that the user has entered.
-                              validator: (value) {
-                                if (value == null || value.isEmpty) {
-                                  return 'Name is Required';
-                                }
-                                return null;
+                              onTap: () {
+                                Navigator.popAndPushNamed(
+                                    context, EventDetails.id);
                               },
-                              onChanged: (value) {
-                                setState(() {
-                                  _name = value;
-                                });
-                              },
-                            ),
-                            SizedBox(
-                              height: 20.0,
-                            ),
-                            TextFormField(
-                              decoration: InputDecoration(
-                                // filled: true,
-                                labelText: 'Email',
-                                labelStyle: TextStyle(
-                                  color: Colors.grey,
-                                ),
-                                enabledBorder: UnderlineInputBorder(
-                                  borderSide: BorderSide(
-                                    color: Colors.grey,
-                                  ),
-                                ),
-                                focusedBorder: UnderlineInputBorder(
-                                  borderSide: BorderSide(
-                                    color: Colors.grey,
-                                  ),
-                                ),
-                              ),
-                              // The validator receives the text that the user has entered.
-                              validator: (value) {
-                                if (value == null || value.isEmpty) {
-                                  return 'Email is Required';
-                                }
-                                return null;
-                              },
-                              onChanged: (value) {
-                                setState(() {
-                                  _email = value;
-                                });
-                              },
-                            ),
-                            SizedBox(
-                              height: 20.0,
-                            ),
-                            TextFormField(
-                              decoration: InputDecoration(
-                                // filled: true,
-                                labelText: 'Mobile Number',
-                                labelStyle: TextStyle(
-                                  color: Colors.grey,
-                                ),
-                                enabledBorder: UnderlineInputBorder(
-                                  borderSide: BorderSide(
-                                    color: Colors.grey,
-                                  ),
-                                ),
-                                focusedBorder: UnderlineInputBorder(
-                                  borderSide: BorderSide(
-                                    color: Colors.grey,
-                                  ),
-                                ),
-                              ),
-                              // The validator receives the text that the user has entered.
-                              validator: (value) {
-                                if (value == null || value.isEmpty) {
-                                  return 'Mobile Number is Required';
-                                }
-                                return null;
-                              },
-                              onChanged: (value) {
-                                setState(() {
-                                  _number = value;
-                                });
-                              },
-                            ),
-                            SizedBox(
-                              height: 20.0,
                             ),
                           ],
                         ),
                       ),
-                    ),
-                    ElevatedButton(
-                      style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all<Color>(
-                          Color(0xff008891),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 30.0),
+                        child: Text(
+                          'Event Register',
+                          style: TextStyle(
+                            fontSize: 30.0,
+                            color: Color(0xff008891),
+                          ),
                         ),
                       ),
-                      child: Text(
-                        'REGISTER',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 16.0,
+                      Form(
+                        key: _formKey,
+                        child: Padding(
+                          padding: const EdgeInsets.all(25.0),
+                          child: Column(
+                            children: [
+                              TextFormField(
+                                decoration: InputDecoration(
+                                  // filled: true,
+                                  labelText: 'Name',
+                                  labelStyle: TextStyle(
+                                    color: Colors.grey,
+                                  ),
+                                  enabledBorder: UnderlineInputBorder(
+                                    borderSide: BorderSide(
+                                      color: Colors.grey,
+                                    ),
+                                  ),
+                                  focusedBorder: UnderlineInputBorder(
+                                    borderSide: BorderSide(
+                                      color: Colors.grey,
+                                    ),
+                                  ),
+                                ),
+                                // The validator receives the text that the user has entered.
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return 'Name is Required';
+                                  }
+                                  return null;
+                                },
+                                onChanged: (value) {
+                                  setState(() {
+                                    _name = value;
+                                  });
+                                },
+                              ),
+                              SizedBox(
+                                height: 20.0,
+                              ),
+                              TextFormField(
+                                decoration: InputDecoration(
+                                  // filled: true,
+                                  labelText: 'Email',
+                                  labelStyle: TextStyle(
+                                    color: Colors.grey,
+                                  ),
+                                  enabledBorder: UnderlineInputBorder(
+                                    borderSide: BorderSide(
+                                      color: Colors.grey,
+                                    ),
+                                  ),
+                                  focusedBorder: UnderlineInputBorder(
+                                    borderSide: BorderSide(
+                                      color: Colors.grey,
+                                    ),
+                                  ),
+                                ),
+                                // The validator receives the text that the user has entered.
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return 'Email is Required';
+                                  }
+                                  return null;
+                                },
+                                onChanged: (value) {
+                                  setState(() {
+                                    _email = value;
+                                  });
+                                },
+                              ),
+                              SizedBox(
+                                height: 20.0,
+                              ),
+                              TextFormField(
+                                decoration: InputDecoration(
+                                  // filled: true,
+                                  labelText: 'Mobile Number',
+                                  labelStyle: TextStyle(
+                                    color: Colors.grey,
+                                  ),
+                                  enabledBorder: UnderlineInputBorder(
+                                    borderSide: BorderSide(
+                                      color: Colors.grey,
+                                    ),
+                                  ),
+                                  focusedBorder: UnderlineInputBorder(
+                                    borderSide: BorderSide(
+                                      color: Colors.grey,
+                                    ),
+                                  ),
+                                ),
+                                // The validator receives the text that the user has entered.
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return 'Mobile Number is Required';
+                                  }
+                                  return null;
+                                },
+                                onChanged: (value) {
+                                  setState(() {
+                                    _number = value;
+                                  });
+                                },
+                              ),
+                              SizedBox(
+                                height: 20.0,
+                              ),
+                            ],
+                          ),
                         ),
                       ),
-                      onPressed: registerUser,
-                    )
-                  ],
+                      ElevatedButton(
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all<Color>(
+                            Color(0xff008891),
+                          ),
+                        ),
+                        child: Text(
+                          'REGISTER',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16.0,
+                          ),
+                        ),
+                        onPressed: registerUser,
+                      )
+                    ],
+                  ),
                 ),
-              ),
+        ),
       ),
     );
   }
